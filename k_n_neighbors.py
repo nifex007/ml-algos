7d648959b990894fdd7986418c4887ab197156c0
Nifemi <nsolaojo@guineainsurance.com>
2018-03-22 20:58:59 +0100

    reshaped and predicted either benign(2) or malignant(4)

--- k_n_neighbors.py
+++ k_n_neighbors.py
@@ -19,3 +19,23 @@ classifier.fit(X_train, y_train)
 
 accuracy = classifier.score(X_test, y_test)
 print(accuracy)
+
+# #  Attribute                     Domain
+#    -- -----------------------------------------
+#    1. Sample code number            id number //this column is dropped and not passed along side args into np.array()
+#    2. Clump Thickness               1 - 10
+#    3. Uniformity of Cell Size       1 - 10
+#    4. Uniformity of Cell Shape      1 - 10
+#    5. Marginal Adhesion             1 - 10
+#    6. Single Epithelial Cell Size   1 - 10
+#    7. Bare Nuclei                   1 - 10
+#    8. Bland Chromatin               1 - 10
+#    9. Normal Nucleoli               1 - 10
+#   10. Mitoses                       1 - 10
+
+# predict either 2. benign or 4. malignant
+example_measures = np.array([2,3,4,5,6,4,3,5,6])
+example_measures = example_measures.reshape(1,-1)
+prediction = classifier.predict(example_measures)
+print(prediction)
+
