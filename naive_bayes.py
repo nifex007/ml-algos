38e9581418e4d0b0ea02c522f8dd47edcb9369bb
Nifemi <nsolaojo@guineainsurance.com>
2018-03-24 16:55:32 +0100

    added comments and added line to test for model confidence

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,4 +1,5 @@
 # Naive Bayes example
+from sklearn import cross_validation
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
 
@@ -6,12 +7,13 @@ import numpy as np
 X = np.array([[-3,7],[1,5], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-2,7]])
 y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
 
-
+# model
 classifier = GaussianNB()
 
+#train
 classifier.fit(X, y)
-
-
+# check confidence or accuracy of the model
+print(classifier.score(X, y)) 
 
 # Predict
 new_feature = [[1, 2], [3,4]]
